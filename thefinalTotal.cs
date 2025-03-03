c16533faaae4be1eacc4a42a164a2f5d55edfb86
fayec3 <24-09957@g.batstate-u.edu.ph>
2025-03-03 09:29:04 +0800

    Update thefinalTotal.cs

--- thefinalTotal.cs
+++ thefinalTotal.cs
@@ -1,20 +1,18 @@
 using System;
 
-int total = 10; // Declare total
+int total = 10;
 int num;
 
 Console.Write("Enter an integer: ");
 num = Convert.ToInt32(Console.ReadLine());
 
-// Loop until the user enters 0
 while (num != 0)
 {
-    // Check if the number is negative and prompt again
     if (num < 0)
     {
         Console.Write("Enter an integer: ");
-        num = Convert.ToInt32(Console.ReadLine()); // Ask again
-        continue; // Skip the rest of the loop and re-check input
+        num = Convert.ToInt32(Console.ReadLine()); 
+        continue; 
     }
 
     if (num % 2 == 0)
@@ -30,7 +28,6 @@ while (num != 0)
         Console.WriteLine($"The number is odd.\n{cube}");
     }
 
-    // Ask for a new number (to avoid infinite loop)
     Console.Write("Enter an integer: ");
     num = Convert.ToInt32(Console.ReadLine());
 }
